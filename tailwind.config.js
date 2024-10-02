5c00cdee3b1d5c39ad3842e16b4903247c4595a8
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2024-10-02 18:36:44 +0530

    neomorphism

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// Tailwind CSS Config
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#f3f4f6",
+          DEFAULT: "#e5e7eb",
+          dark: "#374151",
+        },
+        text: {
+          light: "#1f2937",
+          DEFAULT: "#4b5563",
+          dark: "#d1d5db",
+        },
+        button: {
+          light: "#e5e7eb",
+          dark: "#1f2937",
+        }        
+      },
+    },
+  },
+};
